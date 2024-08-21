0010d1ac026f3210c48a94d9cec03c608b51e686
Yel Yan <yelyan.researchandproduction@gmail.com>
2024-08-22 02:41:31 +0630

    [add] login page UI changes for err message & input bg color

--- src/components/form-kit/login/LoginForm.tsx
+++ src/components/form-kit/login/LoginForm.tsx
@@ -1,7 +1,91 @@
-import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useNavigate } from "react-router-dom";
 
-const LoginForm = () => {
-  return <div>LoginForm</div>;
-};
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+
+// Define schema with both email and password validation
+const FormSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters long",
+  }),
+});
+
+function LoginForm() {
+  const navigate = useNavigate();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="password" placeholder="Password" {...field} />
+              </FormControl>
+              <FormDescription
+                className="text-blue-500 cursor-pointer"
+                onClick={() => navigate("/reset-password")}
+              >
+                Forgot your Password?
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="text-center">
+          <Button type="submit" variant="welcome" className="w-full">
+            Sign In
+          </Button>
+          <p className="text-center">Create new account</p>
+        </div>
+      </form>
+    </Form>
+  );
+}
 
 export default LoginForm;
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { CardShared } from "@/components/shared/CardShared";
+import { AuthLogo } from "@/components/shared/AuthLogo";
+import { AuthBg } from "@/components/shared/AuthBg";
+import LoginForm from "@/components/form-kit/login";
+import { Card, CardContent } from "@/components/ui/card";
+
+const LoginComp = () => {
+  return (
+    <Card className="shadow-lg rounded-lg  border border-gray-100">
+      <CardContent className=" p-4">
+        <div className="text-center">
+          <h1 className="text-black text-2xl font-bold">Welcome Back</h1>
+          <p className="my-4">
+            Please eneter your account details to sign in your account
+          </p>
+        </div>
+        <LoginForm />
+      </CardContent>
+    </Card>
+  );
+};
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <AuthBg>
+      <CardShared logo={<AuthLogo />} cardContent={<LoginComp />} />
+    </AuthBg>
+  );
 };
 
 export default Login;
