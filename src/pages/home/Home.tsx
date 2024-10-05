6ea445464d8a4e781d5bf4a08575d857ef161a5d
Yel yan <yelyan.researchandproduction@gmail.com>
2024-10-05 17:19:43 +0630

    [add] useQuery api integration demo

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,25 +1,35 @@
 import BrandsHomeComp from "./components/ProductsHomeComp";
 import ProductsHomeComp from "./components/BrandsHomeComp";
 import HeroSection from "./components/HeroSection";
-import { useApiGet } from "@/utils/useQueryHook";
+// import { useApiGet } from "@/utils/useQueryHook";
+import { useQuery } from "@tanstack/react-query";
 import { GetAllResponse } from "@/types/common";
 import { fetchProducts } from "@/api/endpoints/productsApi";
 import { Data } from "@/types/Home/homeTypes";
+import { fetchUsers } from "@/api/endpoints/userApi";
 import { useEffect } from "react";
 import { useAppDispatch } from "@/store/hook";
 import { setMainCategories, setProducts } from "@/store/slices/Home/homeSlice";
 
 const Home = () => {
   const dispatch = useAppDispatch();
-  const { data, isSuccess } = useApiGet<GetAllResponse<Data>>(
-    ["home"],
-    fetchProducts,
-    {
-      enabled: true,
-      refetchOnWindowFocus: true,
-      retry: 1,
-    }
-  );
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
+
+  console.log(data);
+
+  // const { data, isSuccess } = useApiGet<GetAllResponse<Data>>(
+  //   ["home"],
+  //   fetchProducts,
+  //   {
+  //     enabled: true,
+  //     refetchOnWindowFocus: true,
+  //     retry: 1,
+  //   }
+  // );
   useEffect(() => {
     if (isSuccess) {
       dispatch(setMainCategories(data.data.maincategroies));
